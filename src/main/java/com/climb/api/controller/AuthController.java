649c5f879c402f85d35fb9738927c6d67de55f29
pedroh <pedromoraespoderoso@gmail.com>
2026-04-08 01:46:09 -0300

    add: endpoint de redirecionamento e ajuste no pom

--- src/main/java/com/climb/api/controller/AuthController.java
+++ src/main/java/com/climb/api/controller/AuthController.java
@@ -10,7 +10,13 @@ import com.climb.api.service.GoogleOAuthService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.net.URI;
 
@@ -85,5 +91,10 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.FOUND)
                 .header(HttpHeaders.LOCATION, uri.toASCIIString())
                 .build();
+    }
+
+    @GetMapping("/google")
+    public RedirectView googleLogin() {
+        return new RedirectView("/oauth2/authorization/google");
     }
 }
