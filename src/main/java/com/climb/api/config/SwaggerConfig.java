bb8b8d059d8165abde411416581cf3cdf0aefaa6
Joaobds <joaogabrielbdscontato@gmail.com>
2026-04-15 22:58:08 -0300

    Fix: Ajustes de swagger

--- src/main/java/com/climb/api/config/SwaggerConfig.java
+++ src/main/java/com/climb/api/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.climb.api.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +12,21 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI custonOpenAPI(){
-        return new OpenAPI().info(
-                new Info()
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "Bearer JWT";
+
+        return new OpenAPI()
+                .info(new Info()
                         .title("Climb Services")
                         .version("1.0")
-                        .description("Uma API para gerenciamento da Climb")
-        );
+                        .description("API para gerenciamento da Climb"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Insira o access token JWT obtido em POST /auth/login")));
     }
 }
